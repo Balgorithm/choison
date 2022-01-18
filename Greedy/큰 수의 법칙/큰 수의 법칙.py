e5c94f9b9f05a48ed443bce1c5c5700a344151b3
choison <89989648+choisony@users.noreply.github.com>
2022-01-18 12:55:02 +0900

    Update 큰 수의 법칙.py

--- Greedy/큰 수의 법칙/큰 수의 법칙.py
+++ Greedy/큰 수의 법칙/큰 수의 법칙.py
@@ -1,8 +1,9 @@
-import sys
-input = sys.stdin.readline
+###배열의 크기 N, 숫자가 더해지는 횟수 M, 그리고 같은 수는 한번에 연속 k번 초과하여 더해질 수 없다.
 
-N = input()
-#...
+n,m,k = map(int,input().split())
+num_list = list(map(int,input().split()))
+num_list.sort()
 
+answer = num_list[-1] * (m//k)*k + num_list[-2] * (m%k)
 
-
+print(answer)
