0b57ed5bf47b115851b80bc139c094150adc1739
choi sungmin <89989648+choisony@users.noreply.github.com>
2022-01-19 12:41:51 +0900

    숫자 카드 게임(O) 문제 이해 처음에 실패

---
+++ Greedy/숫자 카드 게임.py
@@ -0,0 +1,11 @@
+## n*m 행렬
+
+n,m = map(int,input().split())
+
+answer = 0
+for _ in range(n):
+    d= list(map(int,input().split()))
+    if min(d) > answer:
+        answer = min(d)
+
+print(answer)
