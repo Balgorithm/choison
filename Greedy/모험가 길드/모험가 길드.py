f97b9214992768296930c4a145ea14a9a4d42320
choi sungmin <89989648+choisony@users.noreply.github.com>
2022-01-21 09:02:57 +0900

    모험가 길드(O)

--- Greedy/모험가 길드/모험가 길드.py
+++ Greedy/모험가 길드/모험가 길드.py
@@ -1,1 +1,19 @@
-aa
+# input
+# 5
+# 2 3 1 2 2
+# output
+# 2
+
+n=int(input())
+fear = list(map(int,input().split()))
+
+fear = sorted(fear)
+temp =0
+answer = 0
+for i in fear:
+    temp+=1
+    if temp >= i:
+        answer+=1
+        temp = 0
+
+print(answer)
