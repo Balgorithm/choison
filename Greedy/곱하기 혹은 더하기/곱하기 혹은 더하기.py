9f77f3fc42285177968bf5b01e7c2c095745b1be
choi sungmin <89989648+choisony@users.noreply.github.com>
2022-01-21 12:29:05 +0900

    곱하기 혹은 더하기(O)

--- Greedy/곱하기 혹은 더하기/곱하기 혹은 더하기.py
+++ Greedy/곱하기 혹은 더하기/곱하기 혹은 더하기.py
@@ -1,1 +1,20 @@
-aa
+s = input()
+count_0 = 0
+count_1 = 0
+if s[0] == '0':
+    temp ='0'
+    count_0 =1
+if s[0] == '1':
+    temp='1'
+    count_1 =1
+
+for i in s[1:]:
+    if i != temp:
+        if i == '0':
+            count_0 +=1
+            temp = '0'
+        if i == '1':
+            count_1 +=1
+            temp = '1'
+
+print(min(count_0,count_1))
