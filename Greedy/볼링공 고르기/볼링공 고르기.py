f8bc117839aca213b7f8e150b465e26f0f789beb
choi sungmin <89989648+choisony@users.noreply.github.com>
2022-01-24 16:03:15 +0900

    볼링공 고르기(O)
    n이 10만 이상일경우 시간초과가 날 수도 있음

--- Greedy/볼링공 고르기/볼링공 고르기.py
+++ Greedy/볼링공 고르기/볼링공 고르기.py
@@ -1,1 +1,22 @@
-a
+## input
+## 5 3
+## 1 3 2 3 2
+## output
+## 8
+
+## input
+## 8 5
+## 1 5 4 3 2 4 5 2
+## output
+## 25
+
+n,m = map(int,input().split())
+ball = sorted(list(map(int,input().split())))
+
+## 1 2 2 3 3
+cnt = 0
+for i in range(n-1):
+    for j in range(i+1,n):
+       if ball[i] != ball[j]:
+           cnt+=1
+print(cnt)
