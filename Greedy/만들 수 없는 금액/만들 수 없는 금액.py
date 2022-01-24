a91a5bbb38ecc06aa3684ffac3e2b375d6d1473c
choi sungmin <89989648+choisony@users.noreply.github.com>
2022-01-24 12:48:34 +0900

    만들 수 없는 금액(힌트 얻음)

--- Greedy/만들 수 없는 금액/만들 수 없는 금액.py
+++ Greedy/만들 수 없는 금액/만들 수 없는 금액.py
@@ -1,1 +1,17 @@
+## input
+## 1 1 2 3 9
+## output
+## 8
 
+n = int(input())
+coin = sorted(list(map(int,input().split())))
+
+enable = 1
+
+for i in coin:
+    if i > enable:
+        break
+    else:
+        enable += i
+
+print(enable)
