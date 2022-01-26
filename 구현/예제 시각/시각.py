cc7906d68875b0192ea89799bf1328bb180b2a61
choi sungmin <89989648+choisony@users.noreply.github.com>
2022-01-26 11:18:23 +0900

    시각(O)

--- 구현/예제 시각/시각.py
+++ 구현/예제 시각/시각.py
@@ -1,1 +1,11 @@
+### input 5
+### output 11475
+n= int(input())
+answer=0
+for i in range(0,n+1):
+    for j in range(0,60):
+        for k in range(0,60):
+            if '3' in str(i) or '3' in str(j) or '3' in str(k):
+                answer+=1
 
+print(answer)
